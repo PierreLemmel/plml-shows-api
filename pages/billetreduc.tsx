255f70391de9dc7997d81f461528baa3b80e39fa
Pierre Lemmel <pierre.lemmel@gmail.com>
2023-02-13 13:46:26 +0100

    WIP

--- pages/billetreduc.tsx
+++ pages/billetreduc.tsx
@@ -21,6 +21,7 @@ export default function BilletReduc(props: InferGetStaticPropsType<typeof getSta
     const { reviews, billetReducUrl } = props;
 
     const [index, setIndex] = useState<number>(0);
+    const review = reviews[index];
 
     const onRegenerate = () => {
         
@@ -35,22 +36,39 @@ export default function BilletReduc(props: InferGetStaticPropsType<typeof getSta
     useEffect(onRegenerate, []);
 
     const onCopied = () => {
-        console.log("Copied")
+        navigator.clipboard.writeText(review)
     }
 
     const onAddToBR = () => {
         console.log(billetReducUrl)
     }
 
-    return <div className={`fullscreen center-child
+    return <div className="fullscreen center-child
         bg-gradient-to-r from-gray-900 to-gray-800
         text-gray-200 font-mono
-    `}>
-        <div className="flex flex-col items-center justify-between w-3/4 h-3/4 bg-slate-700/50 p-12 rounded-2xl">
-            <div className="w-full h-2/3 center-child text-2xl text-center leading-relaxed">{reviews[index]}</div>
-            <div className="w-full flex flex-row items-center justify-around">
+    ">
+        <div className="flex flex-col items-center justify-between
+            rounded-2xl w-3/4 h-3/4 bg-slate-700/50
+            px-12 py-6
+        ">
+            <div className="
+                w-full text-center my-4
+                text-6xl font-extrabold
+            ">
+                Générateur de critiques aléatoires
+            </div>
+            <div className="
+                w-full center-child italic text-center overflow-y-auto overflow-x-visible
+                max-h-[66%]
+                text-2xl leading-relaxed
+            ">
+                {review}
+            </div>
+            <div className="
+                w-full flex flex-row items-center justify-around
+            ">
                 <Button onClick={onRegenerate}>
-                    Régénérer
+                    Une autre !
                 </Button>
                 <Button onClick={onCopied}>
                     Copier
@@ -71,7 +89,7 @@ const Button = (props: { children: string, onClick: () => void }) => <div
         text-center text-xl font-bold
         py-3 px-6 min-w-[8em]
         bg-gradient-to-r from-cyan-500 to-blue-500
-        hover:hue-rotate-30 hover:cursor-pointer
+        hover:hue-rotate-30 hover:cursor-pointer hover:scale-105
         transition duration-300 rounded-md
     `}
     onClick={props.onClick}
