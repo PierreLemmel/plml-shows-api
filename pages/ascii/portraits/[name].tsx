80268ae861b737df53f3e04b1100f72f24be437e
Pierre Lemmel <Pierre.Lemmel@gmail.com>
2023-06-18 18:52:12 +0200

    WIP

--- pages/ascii/portraits/[name].tsx
+++ pages/ascii/portraits/[name].tsx
@@ -2,7 +2,6 @@ import { AleasButton } from '@/components/aleas/aleas-buttons';
 import AleasHead from '@/components/aleas/aleas-head'
 import AleasSlider from '@/components/aleas/aleas-slider';
 import AsciiArt, { AsciiArtRef, AsciiBitmapStats } from '@/components/ascii/ascii-art';
-import DmxSlider from '@/components/dmx/dmx-slider';
 import { inverseLerp } from '@/lib/services/core/mathf';
 import { mean } from '@/lib/services/core/stats';
 import { RgbColor } from '@/lib/services/core/types';
@@ -15,6 +14,9 @@ const TestAscii = () => {
 	const router = useRouter();
 	const name = router.query["name"] as string;
 
+	const [threshold, setThreshold] = useState<number>(20);
+	const [floor, setFloor] = useState<number>(50);
+
 	const text = useMemo(() => sequence(1000).map(() => randomBool() ? 1 : 0).join(""), [])
 	const colorTransformer = useCallback((color: RgbColor, stats: AsciiBitmapStats) => {
 
@@ -22,22 +24,23 @@ const TestAscii = () => {
 		const { r, g, b } = color;
 		const gray = mean(r, g, b);
 
-		const a0 = 20;
-		const a1 = 100;
 
-		if (gray < a0) {
+		if (gray < threshold) {
 			return RgbColor.black;
 		}
 		else {
-			const framed = 255 * inverseLerp(gray, min, max);
+			const framed = floor + (255 - floor) * inverseLerp(gray, min, max);
 			return RgbColor.grayLevel(framed);
 		}
-	}, []);
+	}, [threshold, floor]);
 
 	const asciiArtRef = useRef<AsciiArtRef>(null);
 
 	const [size, setSize] = useState<number>(5);
 	const [scale, setScale] = useState<number>(5);
+
+	const [baseImageOpacity, setBaseImageOpacity] = useState<number>(0);
+	const [pixelsOpacity, setPixelsOpacity] = useState<number>(0);
 
 	return <>
 		<AleasHead title={`Portrait Ascii${name && ` - ${name}`}`} />
@@ -53,18 +56,18 @@ const TestAscii = () => {
 
 				pixelSize={size}
 				charSize={size - 1}
-				baseImageOpacity={0.05}
-				pixelsOpacity={0.12}
+				baseImageOpacity={baseImageOpacity}
+				pixelsOpacity={pixelsOpacity}
 
 				pixelColorTransformation="none"
 				textColorTransformation={colorTransformer}
 				letterTransformation="framed"
 
-				scale={1}
+				scale={scale}
 			/>
 
 			<div className='left-3 bottom-3 absolute centered-col gap-2'>
-				<div className='text-white'>Size : {size}</div>
+				<div className='text-white'>Size: {size}</div>
 				<AleasSlider
 					orientation='horizontal'
 					className='mb-4'
@@ -73,6 +76,46 @@ const TestAscii = () => {
 					min={1.5}
 					max={10}
 					step={0.5}
+				/>
+				<div className='text-white'>Base image opacity: {baseImageOpacity}</div>
+				<AleasSlider
+					orientation='horizontal'
+					className='mb-4'
+					value={baseImageOpacity}
+					setValue={setBaseImageOpacity}
+					min={0}
+					max={1}
+					step={0.05}
+				/>
+				<div className='text-white'>Pixels Opacity: {pixelsOpacity}</div>
+				<AleasSlider
+					orientation='horizontal'
+					className='mb-4'
+					value={pixelsOpacity}
+					setValue={setPixelsOpacity}
+					min={0}
+					max={1}
+					step={0.05}
+				/>
+				<div className='text-white'>Threshold: {threshold}</div>
+				<AleasSlider
+					orientation='horizontal'
+					className='mb-4'
+					value={threshold}
+					setValue={setThreshold}
+					min={0}
+					max={50}
+					step={2}
+				/>
+				<div className='text-white'>Floor: {floor}</div>
+				<AleasSlider
+					orientation='horizontal'
+					className='mb-4'
+					value={floor}
+					setValue={setFloor}
+					min={0}
+					max={150}
+					step={5}
 				/>
 				<div className='text-white'>Scale : {scale}</div>
 				<AleasSlider
