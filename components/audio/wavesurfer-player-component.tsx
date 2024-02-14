3f3bbaa7e00d1802c72f262e92f9012715f9b28a
Rgeral <rgeral.pro@gmail.com>
2024-02-14 14:06:43 +0100

    Working on addin markers in DB

--- components/audio/wavesurfer-player-component.tsx
+++ components/audio/wavesurfer-player-component.tsx
@@ -16,6 +16,7 @@ export const WaveSurferPlayerComponent: React.FC<WaveSurferPlayerProps> = ({
   const waveformRef = useRef<WaveSurfer | null>(null);
   const [isPlaying, setIsPlaying] = useState(false); // État de la lecture
   const regionsPluginRef = useRef<RegionsPlugin | null>(null);
+  const [markerContent, setMarkerContent] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,7 +28,7 @@ export const WaveSurferPlayerComponent: React.FC<WaveSurferPlayerProps> = ({
         });
         waveformRef.current = waveform;
         const regionsPlugin = waveform.registerPlugin(RegionsPlugin.create());
-        const audioClipData = await getAudioClip("human", "Sinik");
+        const audioClipData = await getAudioClip("human", "Shurik'n Live");
 
         const response = await fetch(audioClipData.url);
         const audioBlob = await response.blob();
@@ -70,12 +71,12 @@ export const WaveSurferPlayerComponent: React.FC<WaveSurferPlayerProps> = ({
     } else SetEnd = 10;
     console.log("SetStart", SetStart);
     console.log("SetEnd", SetEnd);
+    const random = (min: number, max: number) =>
+      Math.random() * (max - min) + min;
+    const randomColor = () =>
+      `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)}, 0.5)`;
 
     if (waveform && regionsPlugin) {
-      const random = (min: number, max: number) =>
-        Math.random() * (max - min) + min;
-      const randomColor = () =>
-        `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)}, 0.5)`;
       console.log("regionPlugin", regionsPlugin);
       console.log("decode");
       regionsPlugin.addRegion({
@@ -88,21 +89,59 @@ export const WaveSurferPlayerComponent: React.FC<WaveSurferPlayerProps> = ({
         color: randomColor(),
       });
     }
+    if (audioClipData.info.markers && regionsPlugin) {
+      const markers: any = audioClipData.info.markers;
+      console.log("markers", markers);
+
+      for (const key in markers) {
+        if (Object.prototype.hasOwnProperty.call(markers, key)) {
+          const value = markers[key];
+          console.log("key", key);
+          console.log("value", value);
+          regionsPlugin.addRegion({
+            id: key,
+            start: value,
+            resize: true,
+            drag: true,
+            content: key,
+            color: randomColor(),
+          });
+        }
+      }
+    }
   };
 
   const handleRegion = async () => {
     const allRegions = regionsPluginRef.current?.getRegions() || [];
-
+    console.log("update");
     if (allRegions.length > 0 && allRegions[0].start != 0.001) {
       const firstRegionStartTime = allRegions[0].start;
       const firstRegionEndTime = allRegions[0].end;
       try {
-        if (clipName) {
+        if (clipName && allRegions.length === 1) {
           await updateAudioClipInfo(
             "human",
-            "Sinik",
+            "Shurik'n Live",
             firstRegionStartTime,
             firstRegionEndTime
+          );
+        }
+        if (clipName) {
+          const markers = new Map<string, number>();
+          console.log("allRegionsLenght", allRegions.length);
+          for (let i = 0; i < allRegions.length; i++) {
+            if (allRegions[i].id !== "MyRegion") {
+              console.log("i number", i);
+              markers.set(allRegions[i].content, allRegions[i].start);
+            }
+          }
+          console.log("markers before", markers.size);
+          await updateAudioClipInfo(
+            "human",
+            "Shurik'n Live",
+            firstRegionStartTime,
+            firstRegionEndTime,
+            markers
           );
         }
       } catch (e) {
@@ -116,7 +155,7 @@ export const WaveSurferPlayerComponent: React.FC<WaveSurferPlayerProps> = ({
     const regionsPlugin = regionsPluginRef.current;
     const waveform = waveformRef.current;
     const allRegions = regionsPluginRef.current?.getRegions() || [];
-
+  
     const random = (min: number, max: number) =>
       Math.random() * (max - min) + min;
     const randomColor = () =>
@@ -126,17 +165,16 @@ export const WaveSurferPlayerComponent: React.FC<WaveSurferPlayerProps> = ({
         id: "Marker",
         drag: true,
         start: 10,
-        content: "test",
+        content: markerContent, 
         color: randomColor(),
       });
     }
-    console.log("allRegions", allRegions);
   };
-
+  
   const removeMarker = () => {
     const allRegions = regionsPluginRef.current?.getRegions() || [];
     for (let i = 0; i < allRegions.length; i++) {
-      if (allRegions[i].id === "Marker") {
+      if (allRegions[i].id != "MyRegion") {
         allRegions[i].remove();
       }
     }
@@ -160,6 +198,12 @@ export const WaveSurferPlayerComponent: React.FC<WaveSurferPlayerProps> = ({
       <button onClick={handlePause}> Pause</button> ||
       <button onClick={handleRegion}> Update Region</button> ||
       <button onClick={handleMakeMarker}> Make Marker</button> ||
+      <input
+        type="text"
+        value={markerContent}
+        onChange={(e) => setMarkerContent(e.target.value)}
+        placeholder="Enter marker content"
+      />
       <button onClick={removeMarker}> Remove Marker</button>
       <div id="waveform" />
     </div>
